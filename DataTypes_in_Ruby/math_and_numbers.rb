1eee241b204dbc4c9d28f6f799a190a7ec182684
TaghiyevMahammad <tagiyevmahammad0@gmail.com>
2025-07-17 11:51:25 +0400

    I have learned about arithmetic operators and some math functions

---
+++ DataTypes_in_Ruby/math_and_numbers.rb
@@ -0,0 +1,19 @@
+num = 20
+puts ("My fav num is " + num.to_s) #to_s stringə çevirmək üçündür
+num2 = -20
+puts num2.abs() #modulunu çap edir
+num3 = 19.457
+puts num3.round() #yuvarlaqlaşdırır
+num4 = 20.1
+puts num4.ceil() #çox az keçibsə belə digər tam ədədi yazır
+num5 = 20.9
+puts num4.floor() #nə qədər yaxınlaşsa da tam hissəsini çap edir
+
+####################################################
+
+puts Math.sqrt(36) #kökə salır
+puts Math.log(8,2) #Loqorifmanı tapır (ilk rəqəm qiyməti,ikinci isə hansı əsasdan olduğunu göstərir)
+puts 10 / 7 #tam hissəsini yazır
+puts 10 / 7.0 #dəqiqliklə bölür
+puts 10 ** 2 #qüvvəti tapır
+puts 10 % 3 #qalığı tapır
