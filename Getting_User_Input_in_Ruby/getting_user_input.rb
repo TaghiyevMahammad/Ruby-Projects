600f3c5335d77bd098b3627f4175879766bb5b7a
TaghiyevMahammad <tagiyevmahammad0@gmail.com>
2025-08-13 21:01:03 +0400

    New changes

--- Getting_User_Input_in_Ruby/getting_user_input.rb
+++ Getting_User_Input_in_Ruby/getting_user_input.rb
@@ -1,7 +1,7 @@
 puts "Enter your name:"
 name = gets #input üçün isdifadə olunur
 puts ("Hello " + name + ", you are so cool!")
-#---------------------------------------------------------------
+#------------------------------------------------------------------
 puts "Enter your name:"
 name = gets.chomp() #sonradan yazdığımız hissənin yeni sətrə keçməsinin qarşısını alır
 puts ("Hello " + name + ", you are so cool!")
